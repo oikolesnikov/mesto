7f47851057207b51ee59643dcada2aca5b871cce
Oleg Kolesnikov <oikolesnikov@mail.ru>
2022-12-20 18:47:50 +0300

    third iteration

--- scripts/index.js
+++ scripts/index.js
@@ -4,7 +4,7 @@ let formElement = document.querySelector(".popup__form");
 let titleInput = document.querySelector(".popup__input_text_title"); 
 let subtitleInput = document.querySelector(".popup__input_text_subtitle");
 
-let editProfile = document.querySelector(".profile__edit-button_about"); 
+let editProfile = document.querySelector(".profile__edit-button"); 
 let profileTitle = document.querySelector(".profile__title");
 let profileSubtitle = document.querySelector(".profile__subtitle");
 
